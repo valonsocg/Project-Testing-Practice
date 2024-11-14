15181bc6b81e749421b88d7d0727296d9927ed63
alonso-dev <valonsocg@gmail.com>
2024-11-14 11:28:33 -0500

    add caesar cipher with test

--- src/testing/caesarCipher.js
+++ src/testing/caesarCipher.js
@@ -1,1 +1,35 @@
-export default function caesarCipher(str) {}
+export default function caesarCipher(str, shift) {
+  const lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
+  const upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //for negetive shift or more than 26 shifts
+  shift = ((shift % 26) + 26) % 26;
+
+  let shiftedLowerAlpha = "";
+  let shiftedUpperAlpha = "";
+
+  //shift the entire alphabet
+  for (let i = 0; i < lowerAlpha.length; i++) {
+    let offset = (i + shift) % lowerAlpha.length;
+    shiftedLowerAlpha += lowerAlpha[offset];
+    shiftedUpperAlpha += upperAlpha[offset];
+  }
+
+  //encode the string
+  let encodedString = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (lowerAlpha.includes(char)) {
+      let index = lowerAlpha.indexOf(char);
+      encodedString += shiftedLowerAlpha[index];
+    } else if (upperAlpha.includes(char)) {
+      let index = upperAlpha.indexOf(char);
+      encodedString += shiftedUpperAlpha[index];
+    } else {
+      encodedString += char;
+    }
+  }
+
+  return encodedString;
+}
--- src/testing/caesarCipher.test.js
+++ src/testing/caesarCipher.test.js
@@ -1,13 +1,15 @@
+import caesarCipher from "./caesarCipher";
+
 describe("caesarCipher", () => {
   test("wrapping from z to a", () => {
     expect(caesarCipher("xyz", 3)).toBe("abc");
   });
 
   test("case preservation", () => {
-    expect(caesarCipher("HeLLo", 3).toBe("KhOOr"));
+    expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
   });
 
   test("test punctuation, space", () => {
-    expect(caesarCipher("Hello, World!", 3).toBe("Khoor, Zruog!"));
+    expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
   });
 });
